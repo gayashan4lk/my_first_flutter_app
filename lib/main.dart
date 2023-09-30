392ba89d856ec52514f51a929708f4a8516c5468
gayashan4lk <gayashan4lk@gmail.com>
2023-09-30 10:02:59 +0530

    add widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Text('Hello World!')));
+  runApp(const MaterialApp(
+      home: Scaffold(body: Center(child: Text("Hello world")))));
 }
