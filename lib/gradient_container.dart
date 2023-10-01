aed172f0b276c4017022601c2cff4bc5b4d7535f
gayashan4lk <gayashan4lk@gmail.com>
2023-10-01 07:00:26 +0530

    pass gradient colors

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -2,14 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:my_first_flutter_app/styled_text.dart';
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer({super.key});
+  const GradientContainer(this.gradientColors, {super.key});
+
+  final List<Color> gradientColors;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [Colors.deepOrange, Colors.purple],
+          colors: gradientColors,
         ),
       ),
       child: const Center(
